6fa55d93889de109ccaec5752cf9ca82f463b820
unknown <joaquingabriel_20@hotmail.com>
2019-12-19 10:33:34 -0600

    proyecto final

---
+++ escuela de codigos/Ciclos/js/script.js
@@ -0,0 +1,89 @@
+// SINTAXIS
+
+// console.log("hola mundo");
+// console.log("hola mundo");
+// console.log("hola mundo");
+// console.log("hola mundo");
+// console.log("hola mundo");
+
+// var i = 0;
+// while (i<850){
+//     console.log("escuela de codigos" + i);
+//     i=i+1;
+// }
+// var i =0;
+// do{
+//     i+=1;
+//     console.log("Carlos" + i);
+// }
+// while(i<8);
+
+// variable booleana
+// var salir = true;
+// // arreglo = conjunto de datos comparte caracteristicas 
+// var numeros = [];
+// // ciclo while que permita mostrar promp indefinido
+// while(salir){
+//   var numero = prompt('ingresa un número');
+  
+//   var seguir = confirm("desea ingresar otro numero");
+//   if(seguir == false ){
+//     salir = false;
+//   }
+// }
+
+// var n=0;
+// while (n<39){
+//   console.log(n + " hola mundo");
+//   n=n+1;
+// }
+
+// var n=0;
+// do{
+//   n=n+1;
+//   console.log(n + " Hola mundo");
+// }
+// while(n<38);
+
+
+// var salir = true;
+// var numeros = [];
+// while(salir){
+//   var numero=prompt("ingresa un numero");
+//   numeros.push(numero);
+//   var seguir = confirm("desea ingresar mas numeros");
+//   if(seguir==false){
+//     salir = false
+//   }
+// }
+// console.log(numeros)
+
+var N1 =prompt("ingresa tu primer numero");
+var N2 =prompt("ingresa tu segundo numero");
+var pre =prompt("Escribe la letra de la operacion: S=suma; R=resta; M=multiplicacion; D=divicion; O=salir");
+if(pre==S){
+  function S(){
+document.write("la suma es  : N1+N2")
+  }
+
+}
+if(pre==R){
+  function R(){
+
+  }
+
+}
+if(pre==M){
+  function M(){
+
+  }
+
+
+}
+if(pre==D){
+  function D(){
+
+  }
+
+}
+function O()
